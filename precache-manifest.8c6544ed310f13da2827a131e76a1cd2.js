3fd339fb47aa02b5414831e4cb18a3f5c8c46ada
Nagarajan N <nagarajan.natarajan@snapwiz.com>
2020-01-07 14:14:16 +0530

    Updates

---
+++ precache-manifest.8c6544ed310f13da2827a131e76a1cd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ad0253a1ff90c3a70655d60423f1a1",
+    "url": "/2d-game/index.html"
+  },
+  {
+    "revision": "ce9a5256a279586dc720",
+    "url": "/2d-game/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6e31d9ce37f6a5767a7c",
+    "url": "/2d-game/static/js/2.e0a22475.chunk.js"
+  },
+  {
+    "revision": "9588f8c48af2196a5c06caf1dd0a6062",
+    "url": "/2d-game/static/js/2.e0a22475.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce9a5256a279586dc720",
+    "url": "/2d-game/static/js/main.faf2064c.chunk.js"
+  },
+  {
+    "revision": "866f24ce31c72fe0ea7f",
+    "url": "/2d-game/static/js/runtime-main.4f713fed.js"
+  },
+  {
+    "revision": "347094ccdc8e4f07f9c5ddd719cc9e8a",
+    "url": "/2d-game/static/media/wheel.347094cc.png"
+  }
+]);
